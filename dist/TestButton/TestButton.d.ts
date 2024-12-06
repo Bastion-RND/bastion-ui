57355fb2224c3619f265f9b03536b8a41db83c48
VladSolyony <solyonyvlad@gmail.com>
2024-12-06 09:50:40 +0300

    тест

--- dist/TestButton/TestButton.d.ts
+++
@@ -1,3 +0,0 @@
-import { FC } from 'react';
-declare const TestButton: FC;
-export { TestButton };
--- dist/bastion-ui.js
+++
@@ -1,5 +0,0 @@
-import { jsx as t } from "react/jsx-runtime";
-const r = () => /* @__PURE__ */ t("button", { style: { color: "var(--ion-color-primary)" }, type: "button", children: "test" });
-export {
-  r as TestButton
-};
