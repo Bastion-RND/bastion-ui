66342f14ae839102adad8af23b10f032becc4fd3
VladSolyony <solyonyvlad@gmail.com>
2024-12-09 17:20:46 +0300

    Поправил warning'и и расширил интерфейс компонента BastButton.tsx

--- lib/BastButton/BastButton.tsx
+++ lib/BastButton/BastButton.tsx
@@ -1,24 +1,26 @@
 import clsx from 'clsx';
-import { FC, PropsWithChildren } from 'react';
+import { ComponentProps, FC, PropsWithChildren } from 'react';
 
 import { capitalize } from '../utils/helpers';
 
-type TBastButtonProps = PropsWithChildren<{
+interface IBastButtonProps extends Omit<ComponentProps<'button'>, 'type'>, PropsWithChildren {
   fill?: 'filled' | 'outlined' | 'cleared';
   size?: 'regular' | 'small';
   color?: 'brand';
   expanded?: boolean;
-}>;
+  type?: 'button' | 'submit';
+}
 
-export const BastButton: FC<TBastButtonProps> = ({
+export const BastButton: FC<IBastButtonProps> = ({
   fill = 'filled',
   size = 'regular',
   color = 'brand',
   expanded = false,
+  type = 'button',
   ...props
 }) => (
   <button
-    type="button"
+    type={type === 'button' ? 'button' : 'submit'}
     className={`bast-button ${clsx(
       {
         [`bast-button--${capitalize(color)}`]: color,
