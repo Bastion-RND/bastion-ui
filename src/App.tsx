e873b81eabe301dac2ce8e430f92da9f18cf49de
VladSolyony <solyonyvlad@gmail.com>
2024-12-10 15:42:12 +0300

    Стилизовал состояние disabled для BastInput

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { BastInput } from '../lib/components/BastInput/BastInput';
 
 const App: FC = () => (
   <>
-    <div style={{ display: 'flex', justifyContent: 'space-between', padding: '10px' }}>
+    <div style={{ display: 'flex', justifyContent: 'space-between', padding: '10px', gap: '10px' }}>
       <BastButton expand='full'>Confirm</BastButton>
       <BastButton fill="outlined" expand='full'>Confirm</BastButton>
       <BastButton fill="cleared" expand='full'>Confirm</BastButton>
@@ -25,8 +25,9 @@ const App: FC = () => (
       <BastButton fill="cleared" size='small'>Confirm</BastButton>
     </div>
 
-    <div style={{ display: 'flex', justifyContent: 'space-between', padding: '10px' }}>
-      <BastInput />
+    <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gap: '10px', padding: '10px' }}>
+      <BastInput value='Злодей' />
+      <BastInput disabled value='Злодей' />
     </div>
   </>
 )
