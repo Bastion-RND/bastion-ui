8b9bde4209e58cf46fe54ffd90b5434343ef813d
VladSolyony <solyonyvlad@gmail.com>
2025-02-24 17:17:50 +0300

    Добавил иконки в header

--- lib/widgets/BastHeader/ui/BastHeader.tsx
+++ lib/widgets/BastHeader/ui/BastHeader.tsx
@@ -1,12 +1,20 @@
 import clsx from 'clsx';
 import { ComponentProps, FC } from 'react';
 
+import { BastIcon } from '../../../entities/BastIcon';
+
 const BastHeader: FC<
   Pick<ComponentProps<'header'>, 'style' | 'className' | 'children' | 'id'> & {
     logo?: 'bast' | 'skat';
   }
-> = ({ logo = 'bast', className, ...props }) => (
-  <header className={`${clsx(['header', className && className])}`} {...props} />
+> = ({ logo = 'skat', className, children, ...props }) => (
+  <header className={`${clsx(['header', className && className])}`} {...props}>
+    <BastIcon
+      className={`${clsx(['header__logo', `header__logo--${logo}`])}`}
+      name={logo === 'skat' ? 'SkatLogoFull' : 'BastLogoFull'}
+    />
+    <div className="header__content">{children}</div>
+  </header>
 );
 
 export { BastHeader };
