49912afc094e8fb2eb29aaf39fd5cc773ac7bd5f
VladSolyony <solyonyvlad@gmail.com>
2025-02-24 17:24:05 +0300

    Добавил stories для новых компонентов

---
+++ src/stories/BastFooter.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { BastFooter as BastFooterComponent } from '../../lib/widgets/BastFooter';
+
+const meta = {
+  title: 'Business/BastFooter',
+  component: BastFooterComponent,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs', 'utility'],
+  args: { children: 'Any content', className: '', style: {}, id: '' },
+} satisfies Meta<typeof BastFooterComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+export const BastFooter: Story = {
+  args: {},
+  argTypes: {},
+};
