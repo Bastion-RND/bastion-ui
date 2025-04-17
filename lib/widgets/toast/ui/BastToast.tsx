aa50a1617a6430f1e93b6542c701bc6cf25f4382
Dmitriy-U <dmitriyushkalov@gmail.com>
2025-04-17 10:26:00 +0300

    Fixed icon in dialog and toast

--- lib/widgets/toast/ui/BastToast.tsx
+++ lib/widgets/toast/ui/BastToast.tsx
@@ -26,7 +26,8 @@ const BastToastWithoutPortal: FC<TBastToastProps> = ({
   const [isVisible, setVisible] = useState<boolean>(true);
   const isVisibleDebounced = useDebounce(isVisible, TOAST_ANIMATION_DURATION);
 
-  const IconByColor = Icons[BAST_ICONS_BY_COLOR[color]] ?? null;
+  const iconByColor = BAST_ICONS_BY_COLOR[color] ?? null;
+  const Icon = iconByColor !== null ? Icons[iconByColor] : null;
 
   const handleClose = () => {
     setVisible(false);
@@ -48,7 +49,7 @@ const BastToastWithoutPortal: FC<TBastToastProps> = ({
 
   return (
     <div className={`${clsx(['toast', `toast--${color}`, isVisible && 'toast--visible'])}`}>
-      {IconByColor !== null && <IconByColor />}
+      {Icon !== null && <Icon />}
       <h5>{text}</h5>
       <BastToastCloseButton onClick={handleClose} />
     </div>
