7ccd5244c8cb21ffde0576eecdbda043ccd02dc3
Dmitriy-U <dmitriyushkalov@gmail.com>
2025-08-04 15:07:07 +0300

    Доделал колонку сетки

--- lib/entities/grid/ui/BastGridColumn.tsx
+++ lib/entities/grid/ui/BastGridColumn.tsx
@@ -1,13 +1,21 @@
 import clsx from 'clsx';
 import { ComponentProps, FC } from 'react';
 
-import { TBastGridColumnBaseProps } from '../config';
+import { TBastGridColumnBaseProps, TGridColumnName, TGridColumnSize } from '../config';
 
 type TBastGridColumnProps = ComponentProps<'div'> & TBastGridColumnBaseProps;
 
-// TODO: Доделать
-const BastGridColumn: FC<TBastGridColumnProps> = ({ className, ...props }) => (
-  <div className={`${clsx(['grid__column', className && className])}`} {...props} />
-);
+const getColumnClassesFromSize = (
+  size: Partial<Record<TGridColumnName, TGridColumnSize>>,
+): string[] =>
+  Object.entries(size).reduce<string[]>((result, [sizeName, sizeValue]) => {
+    result.push(`grid__column--size-${sizeName}-${sizeValue}`);
+    return result;
+  }, []);
+
+const BastGridColumn: FC<TBastGridColumnProps> = ({ size, className, ...props }) => {
+  const cn: string[] = size !== undefined ? ['grid__column', ...getColumnClassesFromSize(size)] : ['grid__column'];
+  return <div className={`${clsx([...cn, className && className])}`} {...props} />;
+};
 
 export { BastGridColumn };
